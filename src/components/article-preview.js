726f7ad4a6c63ee417448a3335fd147eace7292e
Daniel Pham <dnypham@gmail.com>
2022-08-13 20:16:53 -0700

    Refactor user section to UserSection component.

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
 import ArticleImage from "./article-image";
 import ArticleText from "./article-text";
+import UserSection from "./user-section";
 
 export default function ArticlePreview() {
   return (
     <div className="article-card">
       <ArticleImage />
       <ArticleText />
-      <div className="user-section">
-        <img className="user-photo" src="./assets/avatar-michelle.jpg" />
-        <div className="article-information">
-          <p className=" user-name manrope-700 very-dark-grayish-blue">Michelle Appleton</p>
-          <p className="article-date manrope-500 desaturated-dark-blue">28 Jun 2020</p>
-        </div>
-        <button className="share-button-container">
-          <img className="share-button" src="./assets/icon-share.svg" />
-        </button>
-      </div>
+      <UserSection />
     </div>
   )
 }
