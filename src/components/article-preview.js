465c7f376d51b10a8d80f9734b0cfb33fcf92126
Daniel Pham <dnypham@gmail.com>
2022-08-13 18:18:09 -0700

    Add user information and share button.

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -15,6 +15,16 @@ export default function ArticlePreview() {
           you make any room feel complete.
         </p>
       </div>
+      <div className="user-section">
+        <img className="user-photo" src="./assets/avatar-michelle.jpg" />
+        <div className="article-information">
+          <p className=" user-name manrope-700 very-dark-grayish-blue">Michelle Appleton</p>
+          <p className="article-date manrope-500 desaturated-dark-blue">28 Jun 2020</p>
+        </div>
+        <button className="share-button-container">
+          <img className="share-button" src="./assets/icon-share.svg" />
+        </button>
+      </div>
     </div>
   )
 }
