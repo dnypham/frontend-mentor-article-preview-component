cb5579e48d6ba94ff46825932ec6229d45c55222
Daniel Pham <dnypham@gmail.com>
2022-08-13 21:53:01 -0700

    Add functionality to toggle user section and share-section.

--- src/components/user-section.js
+++ src/components/user-section.js
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 
 export default function UserSection() {
+  const [isUserSectionVisible, setIsUserSectionVisible] = useState(true);
+  const [isShareSectionVisible, setIsShareSectionVisible] = useState(false);
+
+  const toggleSection = () => {
+    setIsUserSectionVisible(!isUserSectionVisible);
+    setIsShareSectionVisible(!isShareSectionVisible);
+  }
 
 
   return (
     <>
-    <div className="user-section">
+      <div className={isUserSectionVisible ? 'user-section' : 'hide'}>
       <img className="user-photo" src="./assets/avatar-michelle.jpg" />
       <div className="article-information">
         <p className=" user-name manrope-700 very-dark-grayish-blue">Michelle Appleton</p>
         <p className="article-date manrope-500">28 Jun 2020</p>
       </div>
-      <button className="share-button-container">
+      <button className="share-button-container" onClick={toggleSection}>
         <img className="share-button" src="./assets/icon-share.svg" />
       </button>
     </div>
-    <div className="share-section">
+    <div className={isShareSectionVisible ? 'share-section' : 'hide'}>
       <h4 className="share-text manrope-500 desaturated-dark-blue">SHARE</h4>
        <img className="share-icon" src="./assets/icon-facebook.svg" />
       <img className="share-icon" src="./assets/icon-twitter.svg" />
       <img className="share-icon" src="./assets/icon-pinterest.svg" />
-      <button className="share-button-container-active">
+        <button className="share-button-container-active" onClick={toggleSection}>
         <img className="share-button-active" src="./assets/icon-share.svg" />
       </button>
     </div>
