b240af703279b8a6ccc427127002e99f6de3d79e
Daniel Pham <dnypham@gmail.com>
2022-08-13 21:32:27 -0700

    Add share section.

--- src/components/user-section.js
+++ src/components/user-section.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function UserSection() {
+
+
   return (
+    <>
     <div className="user-section">
       <img className="user-photo" src="./assets/avatar-michelle.jpg" />
       <div className="article-information">
@@ -12,5 +15,15 @@ export default function UserSection() {
         <img className="share-button" src="./assets/icon-share.svg" />
       </button>
     </div>
+    <div className="share-section">
+      <h4 className="share-text manrope-500 desaturated-dark-blue">SHARE</h4>
+       <img className="share-icon" src="./assets/icon-facebook.svg" />
+      <img className="share-icon" src="./assets/icon-twitter.svg" />
+      <img className="share-icon" src="./assets/icon-pinterest.svg" />
+      <button className="share-button-container-active">
+        <img className="share-button-active" src="./assets/icon-share.svg" />
+      </button>
+    </div>
+    </>
   )
 }
