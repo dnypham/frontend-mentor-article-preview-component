ebaa2c4217ef4e513f13a8ba824c41ee72b8e0d5
Daniel Pham <dnypham@gmail.com>
2022-08-13 18:27:34 -0700

    Refactor article text to ArticleText component.

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import ArticleImage from "./article-image";
+import ArticleText from "./article-text";
 
 export default function ArticlePreview() {
   return (
     <div className="article-card">
       <ArticleImage />
-      <div className="article-text-container">
-        <h2 className="article-title manrope-700 very-dark-grayish-blue">
-          Shift the overall look and feel by adding these wonderful touches
-          to furniture in your home
-        </h2>
-        <p className="article-paragraph manrope-500 desaturated-dark-blue">
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I've got some simple tips to help
-          you make any room feel complete.
-        </p>
-      </div>
+      <ArticleText />
       <div className="user-section">
         <img className="user-photo" src="./assets/avatar-michelle.jpg" />
         <div className="article-information">
