006a292a0f6f61b9b60d8c05fa6f1938366c6b0d
Daniel Pham <dnypham@gmail.com>
2022-08-14 21:05:46 -0700

    Refactor output in webpack.config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: path.join(__dirname, '/public/dist'),
+    path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js'
   },
 
