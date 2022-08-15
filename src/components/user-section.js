a01154fd21603a754266d83273fa0908f42ba7df
Daniel Pham <dnypham@gmail.com>
2022-08-14 19:13:09 -0700

    Refactor desktop buttons to show if active.

--- src/components/user-section.js
+++ src/components/user-section.js
@@ -41,14 +41,14 @@ export default function UserSection() {
           <p className=" user-name manrope-700 very-dark-grayish-blue">Michelle Appleton</p>
           <p className="article-date manrope-500">28 Jun 2020</p>
         </div>
-        <button className="share-button-container" onClick={toggleTooltip}>
+        <button className={isDesktopShareButtonActive ? 'share-button-container-active' : 'share-button-container'} onClick={toggleTooltip}>
           <div className={`share-tooltip ${isDesktopShareButtonActive ? 'visible' : 'hidden'}`}>
             <h4 className="share-text manrope-500 desaturated-dark-blue">SHARE</h4>
             <img className="share-icon" src="./assets/icon-facebook.svg" />
             <img className="share-icon" src="./assets/icon-twitter.svg" />
             <img className="share-icon" src="./assets/icon-pinterest.svg" />
           </div>
-          <img className="share-button" src="./assets/icon-share.svg" />
+          <img className={isDesktopShareButtonActive ? 'share-button-active' : 'share-button'} src="./assets/icon-share.svg" />
         </button>
       </div>
     </>
